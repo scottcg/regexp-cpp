921090a584658e7b6f7353df91f75ff0a7cd307e
Scott Garland <2884796+scottcg@users.noreply.github.com>
2024-12-04 11:39:47 -0600

    update tests

--- src/vs/test/test_code_vector.cpp
+++ src/vs/test/test_code_vector.cpp
@@ -1,3 +1,76 @@
 #include <gtest/gtest.h>
+#include <vector>
+#include <algorithm>
+#include "traits.h"
+#include "compile.h"
+#include "code.h"
+#include "tokens.h"
 
-int main() {}
+using ct = re_char_traits<char>;
+using test_code_vector = re::compiled_code_vector<ct>;
+
+namespace re {
+
+    TEST(CompiledCodeVectorTest, Initialization) {
+        const test_code_vector code_vector;
+        EXPECT_EQ(code_vector.offset(), 0);
+    }
+
+    TEST(CompiledCodeVectorTest, StoreSingleCode) {
+        test_code_vector code_vector;
+        const int pos = code_vector.store('A');
+        EXPECT_EQ(pos, 0);
+        EXPECT_EQ(code_vector[0], 'A');
+    }
+
+    TEST(CompiledCodeVectorTest, StoreMultipleCodes) {
+        test_code_vector code_vector;
+        code_vector.store('A');
+        code_vector.store('B');
+        EXPECT_EQ(code_vector[0], 'A');
+        EXPECT_EQ(code_vector[1], 'B');
+    }
+
+    TEST(CompiledCodeVectorTest, ModifyValue) {
+        test_code_vector code_vector;
+        code_vector.store('A');
+        code_vector.store('B');
+        code_vector[1] = 'C';
+        EXPECT_EQ(code_vector[1], 'C');
+    }
+
+    TEST(CompiledCodeVectorTest, PutAddress) {
+        test_code_vector code_vector;
+        code_vector.store('A');
+        code_vector.store('B');
+        code_vector.put_address(1, 10);
+        EXPECT_EQ(static_cast<int>(code_vector[1]), 7);
+        EXPECT_EQ(static_cast<int>(code_vector[2]), 0);
+    }
+
+    TEST(CompiledCodeVectorTest, StoreJump) {
+        test_code_vector code_vector;
+        code_vector.store('A');
+        code_vector.store('B');
+        code_vector.store_jump(0, 1, 5);
+        EXPECT_EQ(static_cast<int>(code_vector[0]), 1);
+        EXPECT_EQ(static_cast<int>(code_vector[1]), 2);
+        EXPECT_EQ(static_cast<int>(code_vector[2]), 0);
+    }
+
+    TEST(CompiledCodeVectorTest, StoreAndRetrieveMultipleValues) {
+        test_code_vector code_vector;
+        for (char c = 'A'; c <= 'Z'; ++c) {
+            code_vector.store(c);
+        }
+        for (char c = 'A'; c <= 'Z'; ++c) {
+            EXPECT_EQ(code_vector[c - 'A'], c);
+        }
+    }
+
+} // namespace re
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ src/vs/test/test_text_pair.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Scott Garland on 12/4/24.
+//
